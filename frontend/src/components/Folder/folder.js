1938e5cb16d09a61459ae6eb05722b66b1a68375
S.Maheswar <maheshcoder999@gmail.com>
2023-08-22 12:05:12 +0530

    update

--- frontend/src/components/Folder/folder.js
+++ frontend/src/components/Folder/folder.js
@@ -6,12 +6,27 @@ const Folder=()=>{
             <form className="form0">
                 <label></label>
             </form>
+
             <form className="form1">
                 <label></label>
             </form>
+
             <form className="form2">
                 <label></label>
             </form>
+
+            <form className="form3">
+                <label></label>
+
+            </form><form className="form4">
+                <label></label>
+
+                
+            </form><form className="form5">
+                <label></label>
+            </form>
+
+
             <div className="text">
                 <h1>Statistics</h1>
                 <p>List six different intresting statistics regarding your data (for example min,max,median,etc.)</p>
