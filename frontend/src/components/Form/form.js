0c61cdc17f861772b975f4db53b486bc5f60c75a
S.Maheswar <maheshcoder999@gmail.com>
2023-08-23 11:05:14 +0530

    update

--- frontend/src/components/Form/form.js
+++ frontend/src/components/Form/form.js
@@ -4,10 +4,11 @@ import "./form.css";
 export const Form = () => {
   return (
     <div className="form">
-      <div className="form-size" />
+      <div className="form-size">
+        <h1 className ="head">About the Data </h1>
+        <p className="type-text">Write ~150 to 200 word introduction to your chosen data set</p>
+      </div>
       <div className="label"/>
-      <p className="type-text">Write ~150 to 200 word introduction to your chosen data set</p>
-  
     </div>
   );
 };
